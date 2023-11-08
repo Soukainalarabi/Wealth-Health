a5b11b247da47f3f11e3dd518659fb2e7153ebfe
Soukaina Larabi <Soukaina.larabi44@gmail.com>
2023-11-08 22:28:47 +0100

    search -table

--- src/pages/EmployeesList.js
+++ src/pages/EmployeesList.js
@@ -1,37 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import {
-  TableEmploye,
-} from 'slarabi-components';
+import { TableEmploye } from 'slarabi-components';
 
 export default function EmployeesList() {
   const [employees, setEmployees] = useState([]);
+  const [searchValue, setSearchValue] = useState(''); // Utilisez un état local pour la valeur de recherche
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
+
   useEffect(() => {
     const storedData = localStorage.getItem('NewEmployee');
-    console.log(storedData);
-    if (storedData) { // Vérifiez si des données existent dans localStorage
+    if (storedData) {
       const parsedData = JSON.parse(storedData);
-      // Si les données sont un tableau, mettez-les à jour dans l'état local
       if (Array.isArray(parsedData)) {
         setEmployees(parsedData);
+        setFilteredEmployees(parsedData);
       }
     }
   }, []);
 
-  return (
-    !employees || employees.length === 0 ? (
-      <div className="container">
-        <h1>No employees found</h1>
-        <Link to="/">Home</Link>
-      </div>
-    ) : (
-      <div className="container">
+  useEffect(() => {
+    const searchValueLower = searchValue.toLowerCase();
+    const filtered = employees.filter(
+      (employee) => employee.firstName.toLowerCase().includes(searchValueLower)
+        || employee.lastName.toLowerCase().includes(searchValueLower)
+        || employee.city.toLowerCase().includes(searchValueLower)
+        || employee.zipCode.toLowerCase().includes(searchValueLower)
+        || employee.stateValue.toLowerCase().includes(searchValueLower)
+        || employee.departmentValue.toLowerCase().includes(searchValueLower),
+    );
+    setFilteredEmployees(filtered);
+  }, [employees, searchValue]);
 
-        {' '}
-        <TableEmploye data={employees} />
-        <Link to="/">Home</Link>
+  return !employees || employees.length === 0 ? (
+    <div className="container">
+      <h1>No employees found</h1>
+      <Link to="/">Home</Link>
+    </div>
+  ) : (
+    <div className="container">
+      <div className="input-search">
+        <label>Search</label>
+        <input
+          type="search"
+          id="search"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+           // Gérez l'événement onChange correctement
+        />
       </div>
-    )
-
+      <TableEmploye data={filteredEmployees} />
+      {filteredEmployees.length !== employees.length ? (
+        <p>
+          Showing 1 to
+          {' '}
+          {filteredEmployees.length}
+          {' '}
+          of
+          {' '}
+          {employees.length}
+          {' '}
+          entries (filtered from
+          {' '}
+          {employees.length}
+          {' '}
+          total entries)
+        </p>
+      ) : (
+        <p>
+          Showing 1 to
+          {' '}
+          {employees.length}
+          {' '}
+          of
+          {' '}
+          {employees.length}
+          {' '}
+          entries
+        </p>
+      )}
+      <Link to="/">Home</Link>
+    </div>
   );
 }
